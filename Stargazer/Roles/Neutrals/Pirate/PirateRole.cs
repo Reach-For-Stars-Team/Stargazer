42fb782c3b6deaeebdb8f5fd25cbbae80adff490
sdfxf31 <sdfxf31@gmail.com>
2026-06-09 22:15:34 +0200

    Update PirateRole.cs

--- Stargazer/Roles/Neutrals/Pirate/PirateRole.cs
+++ Stargazer/Roles/Neutrals/Pirate/PirateRole.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using HarmonyLib;
 using MiraAPI.Events;
 using MiraAPI.Events.Vanilla.Gameplay;
 using MiraAPI.Events.Vanilla.Player;
@@ -10,6 +12,7 @@ using MiraAPI.Roles;
 using MiraAPI.Utilities;
 using Reactor.Utilities.Extensions;
 using Stargazer.Components;
+using Stargazer.Mapping;
 using Stargazer.Utilities;
 using UnityEngine;
 
@@ -77,8 +80,18 @@ public class PirateRole : CrewmateRole, INeutralRole
         if (_mapPlayerDetection) _mapPlayerDetection.gameObject.Destroy();
         _mapPlayerDetection = new GameObject("MapPickup").AddComponent<PlayerDetectionBehaviour>();
         _mapPlayerDetection.canTrigger = true;
-        RandomizationUtils.SpawnObjectRandomly(_mapPlayerDetection.gameObject, new(1.5f, 1.5f));
-        _mapPlayerDetection.transform.position = new Vector3(_mapPlayerDetection.gameObject.transform.position.x, _mapPlayerDetection.gameObject.transform.position.y, 0);
+        RandomizationUtils.SpawnObjectRandomly(
+            _mapPlayerDetection.gameObject,
+            new Vector2(1.5f, 1.5f),
+            true,
+            RandomizationUtils.SpawnAreaFilter.AnyRachable
+        );
+
+        _mapPlayerDetection.transform.position = new Vector3(
+            _mapPlayerDetection.transform.position.x,
+            _mapPlayerDetection.transform.position.y,
+            0f
+        );
         _mapPlayerDetection.OnEnter = p =>
         {
             _mapPlayerDetection.canTrigger = false;
@@ -88,8 +101,18 @@ public class PirateRole : CrewmateRole, INeutralRole
             if (arrow) arrow.gameObject.Destroy();
             xMark = new GameObject("xMark").AddComponent<SpriteRenderer>();
             xMark.sprite = Assets.XMark.LoadAsset();
-            RandomizationUtils.SpawnObjectRandomly(xMark.gameObject, new(2f, 2f));
-            xMark.transform.position = new Vector3(xMark.gameObject.transform.position.x, xMark.gameObject.transform.position.y, 1);
+            RandomizationUtils.SpawnObjectRandomly(
+            xMark.gameObject,
+            new Vector2(1.5f, 1.5f),
+            true,
+            RandomizationUtils.SpawnAreaFilter.ReachableWitchoutRooms
+        );
+
+            xMark.transform.position = new Vector3(
+                xMark.transform.position.x,
+                xMark.transform.position.y,
+                xMark.transform.position.y / 1000f + 0.0009f
+            );
             arrow = Helpers.CreateArrow(HudManager.Instance.transform, RFSPalette.PirateRoleColor);
             arrow.target = xMark.transform.position;
         };
@@ -103,5 +126,102 @@ public class PirateRole : CrewmateRole, INeutralRole
         {
             pirate.IncreaseGold(OptionGroupSingleton<PirateOptions>.Instance.GoldPerTask);
         }
+    }
+}
+
+
+
+[HarmonyPatch(typeof(HudManager), nameof(HudManager.Update))]
+public static class DebugMapSpawner
+{
+    private static readonly List<GameObject> SpawnedObjects = new();
+
+    public static void Postfix()
+    {
+        if (!AmongUsClient.Instance || !AmongUsClient.Instance.AmHost && false) { }
+
+        if (!PlayerControl.LocalPlayer) return;
+        if (!HudManager.Instance) return;
+        if (!ShipStatus.Instance) return;
+
+        if (PlayerControl.LocalPlayer.Data?.Role is not PirateRole)
+            return;
+
+        // if (Input.GetKeyDown(KeyCode.F6))
+        // {
+        //     SpawnDebugMap();
+        // }
+
+        // if (Input.GetKeyDown(KeyCode.F8))
+        // {
+        //     SpawnDebugXMark();
+        // }
+
+        if (Input.GetKeyDown(KeyCode.F7))
+        {
+            ClearDebugObjects();
+        }
+    }
+
+    private static void SpawnDebugMap()
+    {
+        var map = new GameObject("DebugMapSpawn");
+
+        var renderer = map.AddComponent<SpriteRenderer>();
+        renderer.sprite = Assets.Map.LoadAsset();
+
+        RandomizationUtils.SpawnObjectRandomly(
+            map,
+            new Vector2(0.5f, 0.5f),
+            true,
+            RandomizationUtils.SpawnAreaFilter.AnyRachable
+        );
+
+        map.transform.position = new Vector3(
+            map.transform.position.x,
+            map.transform.position.y,
+            map.transform.position.y / 1000f - 0.0009f
+        );
+
+        SpawnedObjects.Add(map);
+
+        HudManager.Instance.SpawnTextOverlay($"Debug objects: {SpawnedObjects.Count}");
+    }
+
+    private static void SpawnDebugXMark()
+    {
+        var xMark = new GameObject("DebugXMarkSpawn");
+
+        var renderer = xMark.AddComponent<SpriteRenderer>();
+        renderer.sprite = Assets.XMark.LoadAsset();
+
+        RandomizationUtils.SpawnObjectRandomly(
+            xMark,
+            new Vector2(1.0f, 1.0f),
+            true,
+            RandomizationUtils.SpawnAreaFilter.ReachableWitchoutRooms
+        );
+
+        xMark.transform.position = new Vector3(
+            xMark.transform.position.x,
+            xMark.transform.position.y,
+            1f
+        );
+
+        SpawnedObjects.Add(xMark);
+
+        HudManager.Instance.SpawnTextOverlay($"Debug objects: {SpawnedObjects.Count}");
+    }
+
+    private static void ClearDebugObjects()
+    {
+        foreach (var obj in SpawnedObjects)
+        {
+            if (obj) obj.Destroy();
+        }
+
+        SpawnedObjects.Clear();
+
+        HudManager.Instance.SpawnTextOverlay("Debug objects cleared");
     }
 }
